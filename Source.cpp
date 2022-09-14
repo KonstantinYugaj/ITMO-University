4d7eac380d37f7572b659260ec2880b0bf6b7c3b
Югай Константин <kostya_yugaj@bk.ru>
2022-09-15 00:21:03 +0300

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b;
+	std::cout << "Введите 2 числа в Лешу\n";
+	std::cin >> a >> b;
+	std::cout<<"Ваш Леша: "<< a + b;
+		return 0;
+
+
+
+}
